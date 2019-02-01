c416fe92c8ffe4ad6d1e4e1538b2cecd3699d684
racsoraul <racsoraul@gmail.com>
2019-01-31 18:19:35 -0600

    Convertion of mouse coordinates working if canvas isn't positioned with styling.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import * as THREE from "three";
 import OrbitControls from "three-orbitcontrols";
-import { Vector3 } from "three";
 import { debounce } from "./utils";
 
 interface IProps {}
@@ -14,6 +13,8 @@ class App extends Component<IProps> {
   private camera!: THREE.PerspectiveCamera;
   private controls!: THREE.OrbitControls;
   private frameId: number;
+  private raycaster: THREE.Raycaster;
+  private mouse: THREE.Vector2;
 
   constructor(props: IProps) {
     super(props);
@@ -25,10 +26,13 @@ class App extends Component<IProps> {
     });
     this.scene = new THREE.Scene();
     this.frameId = 0;
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2(-1, -1);
   }
 
   public componentDidMount() {
     if (this.containerRef.current) {
+      this.containerRef.current.addEventListener("mousemove", this.onMouseMove);
       const containerWidth = this.containerRef.current.clientWidth;
       const containerHeight = this.containerRef.current.clientHeight;
       const aspect = containerWidth / containerHeight;
@@ -40,7 +44,7 @@ class App extends Component<IProps> {
 
       this.camera = new THREE.PerspectiveCamera(45, aspect, 1, 10000);
       this.camera.position.set(0, 0, RANGE * 2);
-      this.camera.lookAt(new Vector3(0, 0, 0));
+      this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
       this.controls = new OrbitControls(this.camera, this.containerRef.current);
       this.controls.enableKeys = false;
@@ -58,9 +62,9 @@ class App extends Component<IProps> {
   public render() {
     return (
       <div className="App">
-        <h1>
-          Object Picking - Test to get the <span>gizmo</span> right
-        </h1>
+        {/* <h1>
+          Object Picking - Test to get the <span>gizmo</span> working
+        </h1> */}
         <div id="gizmo" ref={this.containerRef} />
       </div>
     );
@@ -71,6 +75,15 @@ class App extends Component<IProps> {
    */
   private animate = () => {
     this.frameId = window.requestAnimationFrame(this.animate);
+
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+
+    const intersects = this.raycaster.intersectObjects(this.scene.children);
+    for (let index = 0; index < intersects.length; index++) {
+      console.log(intersects[index].object.position);
+      //console.log(intersects[]);
+    }
+
     this.renderer.render(this.scene, this.camera);
   };
 
@@ -79,6 +92,29 @@ class App extends Component<IProps> {
    */
   private stop() {
     window.cancelAnimationFrame(this.frameId);
+  }
+
+  /**
+   * Returns cube of the specified color. Dimensions are:
+   * `width = 15`
+   * `height = 15`
+   * `depth = 15`
+   * @param color hex number.
+   */
+  private createSimpleCube(color: number = 0xffffff): THREE.Mesh {
+    const geometry = new THREE.BoxGeometry(15, 15, 15);
+    const material = new THREE.MeshBasicMaterial({ color });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    // Add wireframe
+    const edgesGeometry = new THREE.EdgesGeometry(mesh.geometry);
+    const edgesMaterial = new THREE.LineBasicMaterial({
+      color: 0xa25b5b,
+      linewidth: 4
+    });
+    const edgesMesh = new THREE.LineSegments(edgesGeometry, edgesMaterial);
+
+    return mesh.add(edgesMesh);
   }
 
   /**
@@ -95,28 +131,18 @@ class App extends Component<IProps> {
     }
   });
 
-  /**
-   * Returns cube of the specified color. Dimensions are:
-   * `width = 10`
-   * `height = 10`
-   * `depth = 10`
-   * @param color hex number.
-   */
-  private createSimpleCube(color: number = 0xffffff): THREE.Mesh {
-    const geometry = new THREE.BoxGeometry(10, 10, 10);
-    const material = new THREE.MeshBasicMaterial({ color });
-    const mesh = new THREE.Mesh(geometry, material);
-
-    // Add wireframe
-    const edgesGeometry = new THREE.EdgesGeometry(mesh.geometry);
-    const edgesMaterial = new THREE.LineBasicMaterial({
-      color: 0xa25b5b,
-      linewidth: 4
-    });
-    const edgesMesh = new THREE.LineSegments(edgesGeometry, edgesMaterial);
-
-    return mesh.add(edgesMesh);
-  }
+  private onMouseMove = (event: MouseEvent) => {
+    /**
+     * Calculate mouse position in normalized device coordinates
+     * (-1 to +1) for both components.
+     */
+    if (this.containerRef.current) {
+      this.mouse.x =
+        (event.clientX / this.containerRef.current.clientWidth) * 2 - 1;
+      this.mouse.y =
+        -(event.clientY / this.containerRef.current.clientHeight) * 2 + 1;
+    }
+  };
 }
 
 export default App;
